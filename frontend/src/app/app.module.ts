d31095e23ddaa12691aff83b205e3736750ffb98
Carolista <cjones.renaissancewoman@gmail.com>
2020-03-13 22:15:01 -0500

    created header, create-project, and project-details components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProjectComponent } from './project/create-project/create-project.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectDetailsComponent } from './project/project-details/project-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProjectComponent,
+    HeaderComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // brings over info from app-routing.module.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
