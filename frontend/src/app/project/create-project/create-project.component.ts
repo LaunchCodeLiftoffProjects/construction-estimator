27a249e2af12f3dd5cf65105a07b8edffe65a97e
Katherine Sweet <katherine.sweet@slu.edu>
2020-04-01 18:00:18 -0500

    worked through the frontend side of Blake's example API

--- frontend/src/app/project/create-project/create-project.component.ts
+++ frontend/src/app/project/create-project/create-project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../project';
 
 @Component({
   selector: 'app-create-project',
@@ -10,6 +11,25 @@ export class CreateProjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  saveItem(name: string, type: string, length: number, width: number, height: number) {
+    let project = new Project(name, type, length, width, height);
+    console.log("saved project", project);
+    // TODO: POST TO SERVER
+    fetch('http://localhost:8080/api/project', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log('Success:', data);
+    }).catch(function(error) {
+      console.error('Error:', error);
+    });
   }
 
 }
