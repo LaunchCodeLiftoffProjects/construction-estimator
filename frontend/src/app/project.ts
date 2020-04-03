fe5deb7977195e15ab1cda605f7bb476c45ad513
Carolista <cjones.renaissancewoman@gmail.com>
2020-04-02 20:29:34 -0500

    saved ts file (oops)

--- frontend/src/app/project.ts
+++ frontend/src/app/project.ts
@@ -7,7 +7,7 @@ export class Project {
     roomWidth: number;
     roomHeight: number;
 
-    constructor(id: number, name: string, roomType: string, roomLength: number, roomWidth: number, roomHeight: number) {
+    constructor(name: string, roomType: string, roomLength: number, roomWidth: number, roomHeight: number) {
         this.name = name;
         this.roomType = roomType;
         this.roomLength = roomLength;
