546a4b4f6531e2d1b684975c2fd3f039bfd00219
Shaun Minnihan <spminnihan@gmail.com>
2020-04-18 14:42:06 -0500

    Renamed all instances of ProjectComponent to ItemDetails

--- backend/src/main/java/org/launchcode/constructionestimator/controllers/ItemDetailsController.java
+++ backend/src/main/java/org/launchcode/constructionestimator/controllers/ItemDetailsController.java
@@ -1,8 +1,8 @@
 package org.launchcode.constructionestimator.controllers;
 
 import org.launchcode.constructionestimator.models.Project;
-import org.launchcode.constructionestimator.models.ProjectComponent;
-import org.launchcode.constructionestimator.models.data.ProjectComponentRepository;
+import org.launchcode.constructionestimator.models.ItemDetails;
+import org.launchcode.constructionestimator.models.data.ItemDetailsRepository;
 import org.launchcode.constructionestimator.models.data.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,24 +13,24 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/project/{projectId}/component")
-public class ProjectComponentController {
+public class ItemDetailsController {
 
     @Autowired
     ProjectRepository projectRepository;
 
     @Autowired
-    ProjectComponentRepository projectComponentRepository;
+    ItemDetailsRepository itemDetailsRepository;
 
     // Add a list of components
     @PostMapping
-    public ResponseEntity postProjectComponents(@RequestBody List<ProjectComponent> components, @PathVariable("projectId") int projectId) {
+    public ResponseEntity postProjectComponents(@RequestBody List<ItemDetails> components, @PathVariable("projectId") int projectId) {
 
         if(projectRepository.findById(projectId).isEmpty()) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         } else {
             Project project = projectRepository.findById(projectId).get();
-            projectComponentRepository.saveAll(components);
-            project.setProjectComponents(components);
+            itemDetailsRepository.saveAll(components);
+            project.setItemDetails(components);
             projectRepository.save(project);
             return new ResponseEntity(HttpStatus.CREATED);
         }
