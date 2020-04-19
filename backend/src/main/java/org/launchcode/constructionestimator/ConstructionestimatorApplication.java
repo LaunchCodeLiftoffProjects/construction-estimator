257306119aca9af29b511305f65252f995c1801e
Katherine Sweet <katherine.sweet@slu.edu>
2020-04-19 04:54:42 -0500

    Edited the backend Item class so that I could check for items that were associated with one or more rooms. Also updated the project-details and create-project components but was unable to get the routing to work out so that we could use the project id.

--- backend/src/main/java/org/launchcode/constructionestimator/ConstructionestimatorApplication.java
+++ backend/src/main/java/org/launchcode/constructionestimator/ConstructionestimatorApplication.java
@@ -11,6 +11,7 @@ public class ConstructionestimatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConstructionestimatorApplication.class, args);
+
 	}
 
 	@Bean
--- frontend/src/app/project/create-project/create-project.component.ts
+++ frontend/src/app/project/create-project/create-project.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../project';
 
+import { Router, RouterModule, ActivatedRoute, ParamMap, NavigationExtras } from '@angular/router';
+
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent implements OnInit {
+  projectId: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    
   }
 
   saveProject(name: string, roomType: string, roomLength: number, roomWidth: number, roomHeight: number) {
@@ -27,13 +31,15 @@ export class CreateProjectComponent implements OnInit {
       body: JSON.stringify(project),
     }).then(function(response) {
       // get id number from response here { id: idNumber }
-      return response.json();
+      response.json().then(function(json) {
+        json.forEach(element => {
+          this.projectId = Number(element.id);
+        });
+      })
     }).then(function(data) {
       console.log('Success:', data);
     }).catch(function(error) {
       console.error('Error:', error);
     });
-    
   }
-
 }
