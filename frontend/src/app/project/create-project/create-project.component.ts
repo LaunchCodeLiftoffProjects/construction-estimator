eaf5c7a738b3352786d92c05b9182218e9f1a390
Katherine Sweet <katherine.sweet@slu.edu>
2020-04-19 15:04:10 -0500

    continued working on the create-project component to try to get the id saved, but it still doesn't work

--- frontend/src/app/project/create-project/create-project.component.ts
+++ frontend/src/app/project/create-project/create-project.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from '../../project';
 
 import { Router, RouterModule, ActivatedRoute, ParamMap, NavigationExtras } from '@angular/router';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-create-project',
@@ -9,7 +10,7 @@ import { Router, RouterModule, ActivatedRoute, ParamMap, NavigationExtras } from
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent implements OnInit {
-  id: string;
+  id: number = 12;
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
@@ -19,6 +20,7 @@ export class CreateProjectComponent implements OnInit {
 
   saveProject(name: string, roomType: string, roomLength: number, roomWidth: number, roomHeight: number) {
     let project = new Project(name, roomType, roomLength, roomWidth, roomHeight);
+    let id: number;
     console.log("saved project", project);
     // TODO: POST TO SERVER
     fetch('http://localhost:8080/api/project', {
@@ -32,14 +34,20 @@ export class CreateProjectComponent implements OnInit {
     }).then(function(response) {
       // get id number from response here { id: idNumber }
       response.json().then(function(json) {
-        this.id = json.id;
-        console.log("json id", this.id);
-      })
-    }).then(function(data) {
+        //I tried using a forEach loop and the json wasn't gathering the id. I think it's because the one object isn't stored in an array? Not sure though
+        project.id = Number(json.id);
+        //this project.id gets changed, as it should. I tried using an id field on the component class itself, but I encountered the same issue (see below)
+        console.log("json ids", project.id);
+      }.bind(this));
+    }.bind(this)).then(function(data) {
       console.log('Success:', data);
     }).catch(function(error) {
       console.error('Error:', error);
     });
-    this.router.navigate(["/project/add-details/", this.id]);
+    //This project.id gets set back to its initial value of undefined. The this.id field did the same thing - it changed and then got reset to 12 on this line.
+    console.log('id assigned', project.id);
+    //this router link works when the id is given a value before running the fetch, but occassionally it will send you back to the project/create?roomType=whatever which is weird...
+    this.router.navigate([`project/add-details/${project.id}`]);
   }
+
 }
