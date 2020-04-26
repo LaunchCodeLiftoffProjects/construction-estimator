9154eae217fc3d6dd2f6e3a96bb8c8d0fdb2a16c
Shaun Minnihan <spminnihan@gmail.com>
2020-04-26 12:40:20 -0500

    delete mapping done

--- backend/src/main/java/org/launchcode/constructionestimator/controllers/ProjectController.java
+++ backend/src/main/java/org/launchcode/constructionestimator/controllers/ProjectController.java
@@ -1,6 +1,8 @@
 package org.launchcode.constructionestimator.controllers;
 
+import org.launchcode.constructionestimator.models.ItemDetails;
 import org.launchcode.constructionestimator.models.Project;
+import org.launchcode.constructionestimator.models.data.ItemDetailsRepository;
 import org.launchcode.constructionestimator.models.data.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,9 @@ public class ProjectController {
 
     @Autowired
     ProjectRepository projectRepository;
+
+    @Autowired
+    ItemDetailsRepository itemDetailsRepository;
 
     // Use this with @RequestParam to search all projects by field, leave params empty to return all projects
     @GetMapping
@@ -43,6 +48,29 @@ public class ProjectController {
         Map<String, String> map = Collections.singletonMap("id", id.toString());
         return new ResponseEntity(map, HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity deleteProject(@PathVariable("projectId") int projectId) {
+
+        // Check and see if project exists
+        if(projectRepository.findById(projectId).isEmpty()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            Project project = projectRepository.findById(projectId).get();
+
+            // Need to delete all ItemDetails entities associated with project
+            for(ItemDetails itemDetails : project.getItemDetails()) {
+                itemDetailsRepository.deleteById(itemDetails.getId());
+            }
+
+            // lastly delete the project
+            projectRepository.deleteById(projectId);
+
+            return new ResponseEntity(HttpStatus.NO_CONTENT);  // Best practice response code I think
+        }
+
+     }
+
 
 
     // TODO: Delete and put mapping
