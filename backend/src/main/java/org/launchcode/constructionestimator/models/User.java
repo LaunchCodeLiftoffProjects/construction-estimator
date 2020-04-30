2db4de4cb2f6cbcbf8016984ee4955ec61d9b1ce
Shaun Minnihan <spminnihan@gmail.com>
2020-04-29 19:49:19 -0500

    user and UserDetails entities and repositories created

---
+++ backend/src/main/java/org/launchcode/constructionestimator/models/User.java
@@ -0,0 +1,64 @@
+package org.launchcode.constructionestimator.models;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User extends NamedEntity {
+
+    private String FirstName;
+    private String lastName;
+
+    private String password;
+
+    @OneToMany
+    @JoinColumn(name="user_id")
+    private List<Project> projects = new ArrayList<>();
+
+    @OneToOne
+    private UserDetails userDetails;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public UserDetails getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(UserDetails userDetails) {
+        this.userDetails = userDetails;
+    }
+}
