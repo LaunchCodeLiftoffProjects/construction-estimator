5b68059989741c232f9a99dd75492508a92a4662
Carolista <cjones.renaissancewoman@gmail.com>
2020-05-30 16:42:40 -0500

    completed graphics on about page; added link to about page on both header options

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../_services/token-storage.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
+
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  roles: string[] = [];
+  isLoggedIn = false;
+  mySubscription: any;
+
+  constructor(private router: Router, private tokenStorageService: TokenStorageService) {
+   
+   }
 
   ngOnInit() {
+   
+   this.load();
+
+  }
+
+  ngOnDestroy() {
+    if (this.mySubscription) {
+      this.mySubscription.unsubscribe();
+    }
+  }
+
+
+  load() {
+    if (this.tokenStorageService.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorageService.getUser().roles;
+    }
+   
   }
 
 }
